c6589582d07f8413bc3b002889014deed96fd235
DESKTOP-D135NCT\Rian <bisnis.oct@gmail.com>
2020-09-29 14:27:32 +0700

    berhasil hapus data

--- controller.js
+++ controller.js
@@ -64,3 +64,17 @@ exports.ubahMahasiswa = function(req, res){
             }
         });
 }
+
+//menghapus data berdasarkan id
+exports.hapusMahasiswa = function(req, res){
+    var id = req.body.id_mahasiswa;
+
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+        function(error, rows, fields){
+            if (error) { 
+                console.log(error);
+            } else {
+                response.ok("Berhasil hapus data!", res)
+            }
+        });
+}
--- routes.js
+++ routes.js
@@ -17,4 +17,7 @@ module.exports = function(app) {
 
     app.route('/ubah')
         .put(jsonku.ubahMahasiswa);
+
+    app.route('/hapus')
+        .delete(jsonku.hapusMahasiswa);
 }
