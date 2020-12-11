dc83ed03ddeba33a7c2d22c498fdd301fcfac65a
DESKTOP-D135NCT\Rian <bisnis.oct@gmail.com>
2020-12-11 20:33:17 +0700

    registrasi user berhasil

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,4 +6,40 @@ var jwt = require('jsonwebtoken');
 var config = require('../config/secret');
 var ip = require('ip');
 
+// Controller untuk register
+exports.registrasi = function(req, res) {
+    var post = {
+        username: req.body.username,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role,
+        tanggal_daftar: new Date()
+    }
 
+    var query = "SELECT email from ?? WHERE ??=?";
+    var table = ["user", "email" , post.email];
+
+    query = mysql.format(query,table);
+
+    connection.query(query, function(error, rows) {
+        if(error){
+            console.log(error);
+        } else {
+            if(rows.length == 0){
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function(error, rows){
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Berhasil menambahkan data baru", res);
+                    }
+                });
+            } else {
+                response.ok("Email sudah terdaftar!", res);  
+            }
+        }
+    })
+
+}
